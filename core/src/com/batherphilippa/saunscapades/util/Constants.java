8292ad365b524c129613037cd5ea2bc457cd11fb
pabather <p.a.bather1@gmail.com>
2024-03-21 23:14:12 +0100

    added constants

--- core/src/com/batherphilippa/saunscapades/util/Constants.java
+++ core/src/com/batherphilippa/saunscapades/util/Constants.java
@@ -1,2 +1,12 @@
-package com.batherphilippa.saunscapades.util;public class Constants {
+package com.batherphilippa.saunscapades.util;
+
+public class Constants {
+
+    // viewport: virtual height and width for game
+    public static int VIEW_WIDTH = 400;
+    public static int VIEW_HEIGHT = 208;
+
+    // world
+    public static float GRAVITY = -9.8f;
+    public static float PPM = 100; // pixels per meter
 }
