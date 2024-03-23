aeb20bd304aea3cf9df14799c4f91decbf1b9798
pabather <p.a.bather1@gmail.com>
2024-03-23 14:21:25 +0100

    added barrier and coin classes

--- core/src/com/batherphilippa/saunscapades/domain/tilemap/Barrier.java
+++ core/src/com/batherphilippa/saunscapades/domain/tilemap/Barrier.java
@@ -1,2 +1,19 @@
-package com.batherphilippa.saunscapades.domain.tilemap;public class Barrier {
+package com.batherphilippa.saunscapades.domain.tilemap;
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Barrier extends TileObject {
+
+    public Rectangle bounds;
+
+    public Barrier(World world, TiledMap map, Rectangle bounds) {
+        super(world, map, bounds);
+        this.bounds = bounds;
+    }
+
+    public Rectangle getBounds() {
+        return bounds;
+    }
 }
