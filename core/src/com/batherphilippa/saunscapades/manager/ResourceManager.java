694db2527eeaaf9fb21e4fdebc65fb6987b1b6dc
pabather <p.a.bather1@gmail.com>
2024-03-24 00:03:27 +0100

    added resource manager

--- core/src/com/batherphilippa/saunscapades/manager/ResourceManager.java
+++ core/src/com/batherphilippa/saunscapades/manager/ResourceManager.java
@@ -1,2 +1,24 @@
-package com.batherphilippa.saunscapades.manager;public class ResourceManager {
+package com.batherphilippa.saunscapades.manager;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Disposable;
+
+public class ResourceManager implements Disposable {
+
+    private TextureAtlas atlas;
+
+    public ResourceManager() {
+        this.atlas = new TextureAtlas(Gdx.files.internal("atlas/saun_and_enemies.atlas"));
+    }
+
+    public TextureAtlas.AtlasRegion loadRegion(String name) {
+        TextureAtlas.AtlasRegion region = atlas.findRegion(name);
+        return region;
+    }
+
+    @Override
+    public void dispose() {
+        atlas.dispose();
+    }
 }
