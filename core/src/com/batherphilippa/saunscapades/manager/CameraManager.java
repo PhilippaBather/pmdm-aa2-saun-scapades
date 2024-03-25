04d3116ee9484366fc95793e962a4d693ae29552
pabather <p.a.bather1@gmail.com>
2024-03-25 15:18:12 +0100

    refactored

--- core/src/com/batherphilippa/saunscapades/manager/CameraManager.java
+++ core/src/com/batherphilippa/saunscapades/manager/CameraManager.java
@@ -2,6 +2,7 @@ package com.batherphilippa.saunscapades.manager;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -10,12 +11,19 @@ import static com.batherphilippa.saunscapades.util.Constants.*;
 public class CameraManager {
 
     private final OrthographicCamera gameCam;
-
+    private final OrthographicCamera hudCam;
+    private final OrthographicCamera optionCam;
     private final Viewport gamePort;
+    private final Viewport hudPort;
+    private final Viewport optionPort;
 
     public CameraManager() {
         this.gameCam = new OrthographicCamera();
+        this.hudCam = new OrthographicCamera();
+        this.optionCam = new OrthographicCamera();
         this.gamePort = new FitViewport(VIEW_WIDTH / PPM, VIEW_HEIGHT / PPM, gameCam);
+        this.hudPort = new FillViewport(VIEW_WIDTH, VIEW_HEIGHT, new OrthographicCamera());
+        this.optionPort = new FitViewport(VIEW_WIDTH, VIEW_HEIGHT, new OrthographicCamera());
         setCameraPosition();
     }
 
@@ -28,9 +36,34 @@ public class CameraManager {
         return gameCam;
     }
 
+    public OrthographicCamera getHudCam() {
+        return hudCam;
+    }
+
+    public OrthographicCamera getOptionCam() {
+        return optionCam;
+    }
+
+    public Viewport getHudPort() {
+        return hudPort;
+    }
+
+    public Viewport getOptionPort() {
+        return optionPort;
+    }
+
     public void resizeGamePort(int width, int height) {
         gamePort.update(width, height);
     }
+    public void resizeHudPort(int width, int height) {
+        hudPort.update(width, height);
+    }
+    public void resizeOptionPort(int width, int height) {
+        optionPort.update(width, height);
+    }
+
+
+
     public void update(float x) {
         getGameCam().position.x = x; // player.getB2Body().getPosition().x;
         getGameCam().update();
