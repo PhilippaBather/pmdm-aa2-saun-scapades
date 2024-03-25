46b1927a787d6ed34cf611ffeb1f502d0ab7a61d
pabather <p.a.bather1@gmail.com>
2024-03-25 15:18:00 +0100

    added input option manager

--- core/src/com/batherphilippa/saunscapades/manager/OptionManager.java
+++ core/src/com/batherphilippa/saunscapades/manager/OptionManager.java
@@ -1,2 +1,29 @@
-package com.batherphilippa.saunscapades.manager;public class OptionsManager {
+package com.batherphilippa.saunscapades.manager;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.batherphilippa.saunscapades.SaunScapades;
+import com.batherphilippa.saunscapades.screen.GameState;
+import com.kotcrab.vis.ui.widget.VisTextButton;
+
+public class OptionManager {
+
+    public static void handlePauseClicked(VisTextButton btn, SaunScapades game) {
+        btn.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("pause clicked");
+                game.setGameState(GameState.PAUSED);
+            }
+        });
+    }
+
+    public static void handleUnPauseClicked(VisTextButton btn, SaunScapades game) {
+        btn.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("resume game...");
+                game.setGameState(GameState.RUNNING);
+            }
+        });
+    }
+
 }
