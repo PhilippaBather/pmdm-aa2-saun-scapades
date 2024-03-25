e01ecd1bfc520f5c99084cfe4d5db2dc80e1a3c2
pabather <p.a.bather1@gmail.com>
2024-03-25 15:19:01 +0100

    added UI utils

--- core/src/com/batherphilippa/saunscapades/screen/util/UIUtils.java
+++ core/src/com/batherphilippa/saunscapades/screen/util/UIUtils.java
@@ -1,9 +1,18 @@
-package com.batherphilippa.saunscapades.scene.util;
+package com.batherphilippa.saunscapades.screen.util;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.kotcrab.vis.ui.VisUI;
 import com.kotcrab.vis.ui.widget.VisTable;
 
 public class UIUtils {
+
+    public static void clearScreen() {
+        // colour
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        // clears screen
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
 
     public static VisTable createTableObj() {
 
