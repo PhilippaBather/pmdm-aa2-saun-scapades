7e0d7cf519f20603fac33a5704bdf311265c6a09
pabather <p.a.bather1@gmail.com>
2024-03-25 10:42:01 +0100

    updated to manage state and jump music

--- core/src/com/batherphilippa/saunscapades/manager/ResourceManager.java
+++ core/src/com/batherphilippa/saunscapades/manager/ResourceManager.java
@@ -1,24 +1,39 @@
 package com.batherphilippa.saunscapades.manager;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.utils.Disposable;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ResourceManager implements Disposable {
 
     private TextureAtlas atlas;
+    private Map<String, Sound> soundMap;
 
     public ResourceManager() {
         this.atlas = new TextureAtlas(Gdx.files.internal("atlas/saun_and_enemies.atlas"));
+        this.soundMap = new HashMap<>();
+        loadSoundFX();
     }
 
-    public TextureAtlas.AtlasRegion loadRegion(String name) {
-        TextureAtlas.AtlasRegion region = atlas.findRegion(name);
-        return region;
+    private void loadSoundFX() {
+        this.soundMap.put("shaun_jump", Gdx.audio.newSound(Gdx.files.internal("sound/sound/jump-shaun.wav")));
+    }
+
+    public TextureAtlas.AtlasRegion loadRegion(String name, int index) {
+        return atlas.findRegion(name, index);
+    }
+
+    public void playSound(String name) {
+        this.soundMap.get(name).play();
     }
 
     @Override
     public void dispose() {
         atlas.dispose();
+        soundMap.clear();
     }
 }
--- core/src/com/batherphilippa/saunscapades/manager/SpriteManager.java
+++ core/src/com/batherphilippa/saunscapades/manager/SpriteManager.java
@@ -2,11 +2,17 @@ package com.batherphilippa.saunscapades.manager;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.Timer;
 import com.batherphilippa.saunscapades.SaunScapades;
 import com.batherphilippa.saunscapades.domain.sprite.Shaun;
 import com.batherphilippa.saunscapades.util.UserInput;
+
+import static com.batherphilippa.saunscapades.domain.sprite.SpritePositionState.IDLE;
+import static com.batherphilippa.saunscapades.domain.sprite.SpritePositionState.MOVING;
 
 public class SpriteManager implements Disposable {
 
@@ -15,6 +21,8 @@ public class SpriteManager implements Disposable {
     private final ResourceManager resManager;
     private final SpriteBatch batch;
     private Shaun player;
+    private float timeToNextLoop = 2;
+
 
     public SpriteManager(SaunScapades game, SpriteBatch batch) {
         this.game = game;
@@ -29,7 +37,11 @@ public class SpriteManager implements Disposable {
      * Inicializar a los sprites.
      */
     private void init() {
-        this.player = new Shaun(resManager.loadRegion("shaun_walk"), b2WorldManager.getWorld(), 32, 38, 6);
+        this.player = new Shaun(resManager.loadRegion("shaun_walk", 0), b2WorldManager.getWorld(), 32, 38, 6, this);
+    }
+
+    public TextureRegion getTextureRegion(String name, int index) {
+        return resManager.loadRegion(name, index);
     }
 
     public float getPlayerPosition() {
@@ -43,8 +55,8 @@ public class SpriteManager implements Disposable {
     public void manageInput() {
         if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
             player.move(UserInput.UP);
+            resManager.playSound("shaun_jump");
         }
-
         // not just pressed as want to know if the user is holding the key down
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && player.getLinearVelocity() <= 2) {
             player.move(UserInput.RIGHT);
@@ -53,7 +65,6 @@ public class SpriteManager implements Disposable {
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && player.getLinearVelocity() >= -2) {
             player.move(UserInput.LEFT);
         }
-
     }
 
     public void draw() {
