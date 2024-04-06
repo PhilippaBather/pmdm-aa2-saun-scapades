84ae5a42cc0ccd08386129e53b1f13bc2b2da2af
pabather <p.a.bather1@gmail.com>
2024-04-07 00:21:20 +0200

    added water tile object

--- core/src/com/batherphilippa/saunscapades/domain/tilemap/Water.java
+++ core/src/com/batherphilippa/saunscapades/domain/tilemap/Water.java
@@ -1,2 +1,24 @@
-package com.batherphilippa.saunscapades.domain.tilemap;public class Water {
+package com.batherphilippa.saunscapades.domain.tilemap;
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.physics.box2d.World;
+import com.batherphilippa.saunscapades.manager.ResourceManager;
+import com.batherphilippa.saunscapades.screen.scene.Hud;
+
+import static com.batherphilippa.saunscapades.listener.WorldCategoryBits.WATER_BIT;
+
+public class Water extends TileObject {
+    public Water(World world, TiledMap map, Rectangle bounds) {
+        super(world, map, bounds);
+        // vincula los datos del usuario con el objeto sí mismo
+        this.fixture.setUserData(this);
+        setCategoryFilter(WATER_BIT);
+    }
+
+    @Override
+    public void onContact(ResourceManager resourceManager, Hud hud) {
+        hud.updateLives(-1);
+        resourceManager.playSound("sheep_death");
+    }
 }
