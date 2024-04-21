d2c404e7be3ad454757790bbd458a1e46db172bb
pabather <p.a.bather1@gmail.com>
2024-04-21 21:41:35 +0200

    added game level enum

--- core/src/com/batherphilippa/saunscapades/screen/GameLevel.java
+++ core/src/com/batherphilippa/saunscapades/screen/GameLevel.java
@@ -1,6 +1,15 @@
 package com.batherphilippa.saunscapades.screen;
 
 public enum GameLevel {
-    LEVEL_1,
-    LEVEL_2
+    LEVEL_1(1),
+    LEVEL_2(2);
+
+    private int level;
+    GameLevel(int level) {
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
 }
