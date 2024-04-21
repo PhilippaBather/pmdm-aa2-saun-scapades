e98b343e796c442731ab6fcfa822778e33f24078
pabather <p.a.bather1@gmail.com>
2024-04-21 21:32:32 +0200

    updated for second level explosions

--- core/src/com/batherphilippa/saunscapades/domain/sprite/Bomb.java
+++ core/src/com/batherphilippa/saunscapades/domain/sprite/Bomb.java
@@ -3,10 +3,13 @@ package com.batherphilippa.saunscapades.domain.sprite;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Disposable;
+import com.batherphilippa.saunscapades.SaunScapades;
 import com.batherphilippa.saunscapades.manager.SpriteManager;
+import com.batherphilippa.saunscapades.screen.GameLevel;
 import com.batherphilippa.saunscapades.util.UserInput;
 
 import static com.batherphilippa.saunscapades.listener.WorldCategoryBits.BOMB_BIT;
@@ -19,6 +22,8 @@ public class Bomb extends Character implements Disposable {
     private float stateTimer;
     private boolean isDetonated;
     private boolean isDestroyed;
+    private float timeCount;
+    private int detonationTime;
 
     public Bomb(TextureRegion region, World world, float x, float y, float radius, SpriteManager spriteManager) {
         super(region, world, x, y, radius, spriteManager, SpriteType.ENEMY);
@@ -28,12 +33,18 @@ public class Bomb extends Character implements Disposable {
         this.bombBlast = setAnimationFrames("explosion", 1, 2, 0.3f);
 
         initValues();
+        b2Body.setActive(false);
     }
 
     private void initValues() {
         this.stateTimer = 0;
         this.isDetonated = false;
         this.isDestroyed = false;
+
+        if (SaunScapades.currGameLevel == GameLevel.LEVEL_2) {
+            this.timeCount = 0;
+            this.detonationTime = 10;
+        }
     }
 
     @Override
@@ -45,16 +56,30 @@ public class Bomb extends Character implements Disposable {
 
     @Override
     public void update(float delta) {
+        b2Body.setActive(true);
+
+        if (SaunScapades.currGameLevel == GameLevel.LEVEL_2) {
+            levelTwoUpdates(delta);
+        }
+
         this.stateTimer += delta;
         setRegion(getTextureRegion());
+
         if (!isDetonated) {
             setCenter(b2Body.getPosition().x, b2Body.getPosition().y);
-        } else if(!isDestroyed) {
+        } else if (!isDestroyed) {
             world.destroyBody(b2Body);
             isDestroyed = true;
             stateTimer = 0;
         }
+    }
 
+    private void levelTwoUpdates(float delta) {
+        setCountDown(delta);
+
+        if (detonationTime <= 0) {
+            b2Body.applyLinearImpulse(new Vector2(-0.1f, 0), b2Body.getWorldCenter(), true);
+        }
     }
 
     private TextureRegion getTextureRegion() {
@@ -63,6 +88,21 @@ public class Bomb extends Character implements Disposable {
         } else {
             return bombBlast.getKeyFrame(stateTimer, false);
         }
+    }
+
+    public void setCountDown(float delta) {
+        if (!isDetonated) {
+            timeCount += delta;
+            if (timeCount >= 1) { // 1 segundo
+                detonationTime--;
+                timeCount = 0;
+            }
+        }
+    }
+
+
+    public void setDetonated(boolean detonated) {
+        isDetonated = detonated;
     }
 
     @Override
