9f3659c557bf9dd5532857197d056b3c30ad88cc
pabather <p.a.bather1@gmail.com>
2024-04-21 21:43:14 +0200

    added ui constants

--- core/src/com/batherphilippa/saunscapades/screen/constants/UIConstants.java
+++ core/src/com/batherphilippa/saunscapades/screen/constants/UIConstants.java
@@ -10,6 +10,7 @@ public class UIConstants {
     public static final String BTN_PAUSE = "PAUSE";
     public static final String BTN_PLAY = "PLAY";
     public static final String BTN_PLAY_AGAIN = "PLAY AGAIN";
+    public static final String BTN_SAVE = "SAVE";
     public static final String BTN_RESUME = "Resume Game";
 
     public static final String LABEL_GAME_LENGTH = "Select Game Length";
@@ -18,4 +19,9 @@ public class UIConstants {
     public static final String LABEL_GAME_OVER_TITLE = "GAME OVER!!!";
     public static final String LABEL_MAIN_MENU_TITLE = "MAIN MENU";
     public static final String LABEL_OPTIONS_MENU_TITLE = "SELECT CONFIGURATION OPTIONS";
+    public static final String LABEL_MISSION_2 = "Mission 2";
+    public static final String TEXT_AREA_LEVEL_2_INFO = "Save as many Shirley sheep from the bombs.";
+    public static final String TEXT_AREA_LEVEL_2_AVOID = "Avoid falling Timmy sheep.";
+    public static final String TEXT_FIELD_LEVEL_END = "Congratulations: Level Conquered!";
+    public static final String TEXT_FIELD_SCORE = "SCORE: %s";
 }
