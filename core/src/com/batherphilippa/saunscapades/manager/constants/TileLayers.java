e2333b723c8eea04a51b82a6c80c7378b5b0195f
pabather <p.a.bather1@gmail.com>
2024-04-21 21:34:39 +0200

    added constants

--- core/src/com/batherphilippa/saunscapades/manager/constants/TileLayers.java
+++ core/src/com/batherphilippa/saunscapades/manager/constants/TileLayers.java
@@ -1,2 +1,15 @@
-package com.batherphilippa.saunscapades.manager.constants;public class TileLayers {
+package com.batherphilippa.saunscapades.manager.constants;
+
+public class TileLayers {
+    public static final int GRAPHICS_ORNAMENTS = 2;
+    public static final int GROUND_TL = 3;
+    public static final int BARRIERS_TL = 4;
+    public static  final int COINS_TL = 5;
+    public static final int WATER_TL = 6;
+    public static final int ANGRY_SHEEP_TL = 7;
+    public static final int BOMBS_TL = 8;
+    public static final int SHIRLEY_SHEEP_TL = 9;
+    public static final int BLOCKS_TL = 10;
+    public static final int TRAPPED_SHEEP_TL = 11;
+    public static final int FLYING_SHEEP_TL = 12;
 }
