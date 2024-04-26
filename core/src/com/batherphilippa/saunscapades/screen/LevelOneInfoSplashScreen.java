875de20bb0db9165c0a0098f1cdf28aee9a4c25e
pabather <p.a.bather1@gmail.com>
2024-04-26 16:06:11 +0200

    decreased delay

--- core/src/com/batherphilippa/saunscapades/screen/LevelOneInfoSplashScreen.java
+++ core/src/com/batherphilippa/saunscapades/screen/LevelOneInfoSplashScreen.java
@@ -42,7 +42,7 @@ public class LevelOneInfoSplashScreen implements Screen {
 
         stage.addAction(Actions.sequence(Actions.alpha(0),
                 Actions.fadeIn(1.5f),
-                Actions.delay(15f),
+                Actions.delay(12f),
                 Actions.run(new Runnable() {
                     @Override
                     public void run() {
