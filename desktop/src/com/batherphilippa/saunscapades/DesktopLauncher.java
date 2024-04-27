202e3925bf34a0a3467c8cff1adc82d6459d51d6
pabather <p.a.bather1@gmail.com>
2024-04-27 23:15:09 +0200

    added comments

--- desktop/src/com/batherphilippa/saunscapades/DesktopLauncher.java
+++ desktop/src/com/batherphilippa/saunscapades/DesktopLauncher.java
@@ -3,7 +3,9 @@ package com.batherphilippa.saunscapades;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+/**
+ * DesktopLauncher - el punto de entrada del juego de escritorio.
+ */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
